c1761e104a95eb0f81a1054c12598a377a37f0b9
Jxiang-jc <zjx1020645@163.com>
2019-01-15 13:17:52 +0800

    优化代码, 新增打包时忽略jquery和echarts

---
+++ myProject/src/js/echarts.js
@@ -0,0 +1,76 @@
+// common JS
+const echarts = require('echarts')
+
+// ES Module
+import $ from 'jquery'
+// 上面两个区别可看 https://www.cnblogs.com/liaojie970/p/7376682.html
+
+const saleChart = {
+
+    init() {
+        this.initialSaleChart()
+    },
+
+    // 初始化图表
+    initialSaleChart() {
+        // 获取图表容器
+        let main = document.getElementById('main')
+
+        // 图表实例
+        let myChart = echarts.init(main)
+
+        // 获取数据
+        this.getSaleData(myChart)
+    },
+
+    // 异步获取数据
+    getSaleData(myChart) {
+        // 打包以后json文件不能放在最外面, 不然会报错. 所以我放进文件夹里面去了
+        $.get('./json/data.json')
+            .done(res => {
+                console.log(res)
+                this.creatOption(myChart, res)
+            })
+    },
+
+    // 接到图标实例, 数据
+    creatOption(chart, res) {
+
+        // 默认配置
+        let options = {
+            title: {
+                text: 'ECharts实例'
+            },
+            tooltip: {},
+            xAxis: {
+                data: []
+            },
+            yAxis: [{
+                type: 'value'
+            }],
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: []
+            }]
+        }
+
+        // 通过数据更新配置
+        let newOption = Object.assign({}, options, {
+            xAxis: {
+                data: res.cat
+            },
+            series: [{
+                // 根据名字对应到相应的系列
+                name: '销量',
+                type: 'bar',
+                data: res.data
+            }]
+        })
+
+        // 为echarts对象加载数据
+        chart.setOption(newOption)
+    }
+}
+
+export default saleChart
--- myProject/webpack.config.js
+++ myProject/webpack.config.js
@@ -1,8 +1,6 @@
 // webpack v4
 const path = require('path');
-// update from 23.12.2018
-// const nodeExternals = require('webpack-node-externals');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 自动生成index.html
 module.exports = {
     entry: {
         main: './src/index.js'
@@ -11,16 +9,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
-    // target: 'node', // update from 23.12.2018
-    // externals: [nodeExternals()], // update from 23.12.2018
+    // 不打包jq
+    externals: {
+        jquery: 'jQuery',
+        echarts: 'echarts'
+    },
     module: {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
-            exclude: __dirname + 'node_modules',
-            include: __dirname + 'src',
+            exclude: __dirname + 'node_modules', // 不打包的路径
+            include: __dirname + 'src', // 打包的路径
             options: {
-                presets: ['env']
+                presets: ['env'] //这个是ES6 转 ES5的
             }
         }]
     },
